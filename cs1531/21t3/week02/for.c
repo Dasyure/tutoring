e4434c63fff315091066124b136a779e1866d9d0
Daysure <das.yaegar@gmail.com>
2021-09-20 23:48:39 +1000

    created for loop tutorial, and covered part A

---
+++ cs1531/21t3/week02/for.c
@@ -0,0 +1,18 @@
+// Print numbers from 1 to 10
+#include <stdio.h>
+
+int main() {
+    int i;
+    for (i = 0; i < 10; i++) {
+        printf("%d ", i);
+    }
+
+    // int i = 0;
+    // while (i < 10) {
+    //     printf("%d ", i);
+    //     i++;
+    // }
+
+    printf("\n");
+    return 0;
+}
---
+++ cs1531/21t3/week02/for.py
@@ -0,0 +1,7 @@
+for i in range(10):
+    print(f"{i} ", end="")
+print()
+
+word_list = ["My", "dog", "is", "cool"]
+for word in word_list:
+    print(word)

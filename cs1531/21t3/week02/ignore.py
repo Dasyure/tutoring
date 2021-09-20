74fb28e6257063af612596bab4d60365013fc4f4
Daysure <das.yaegar@gmail.com>
2021-09-21 07:47:39 +1000

    creating conflict

--- cs1531/21t3/week02/ignore.py
+++ cs1531/21t3/week02/ignore.py
@@ -3,4 +3,9 @@
 Compute the average of only the positive elements in the list.
 '''
 def rainfall(integers):
-    pass
+    ''' List-comprehension solution '''
+    positive = [i for i in integers if i > 0]
+    if (len(positive) > 0):
+        return sum(positive)/len(positive)
+    else:
+        return None

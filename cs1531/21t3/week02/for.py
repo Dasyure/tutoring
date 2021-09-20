6c200d3f2776b0e90564d503afc41ce83d320174
Daysure <das.yaegar@gmail.com>
2021-09-20 23:51:24 +1000

    created for loop tutorial, and covered part A

--- cs1531/21t3/week02/for.py
+++ cs1531/21t3/week02/for.py
@@ -5,3 +5,7 @@ print()
 word_list = ["My", "dog", "is", "cool"]
 for word in word_list:
     print(word)
+
+word_list = ["My", "dog", "is", "cool"]
+for idx, word in enumerate(word_list):
+    print(f"Index: {idx}, Word: {word}")
